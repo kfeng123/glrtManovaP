810131aed428cb53796b5372fe29b68fdc8e1118
kfeng123 <446100240@qq.com>
2017-06-11 21:16:54 +0800

    NEAR

--- code/test.R
+++ code/test.R
@@ -16,14 +16,16 @@ doit <- function() {
     # C
     tmp <- t(NEW.J)%*%rep(1,sum(n))
     C <- eigen(diag(K)-tmp%*%t(tmp)/sum(n))$vectors[,-K]
-    
+   
+    #SNR 
     tmpMu <- do.call(cbind,mu)
     for(i in 1:K){
         tmpMu[,i] <- tmpMu[,i]*sqrt(n[i])
     }
     tmpMuF <- tmpMu%*%C
         
-    tmpCon <- sqrt(SNR*sqrt(sum(Sigma^2))/sum(tmpMuF^2))
+    tmpSigEig <- eigen(Sigma)$values[-1]
+    tmpCon <- sqrt(SNR*sqrt(sum(tmpSigEig^2))/sum(tmpMuF^2))
     for(i in 1:length(mu)){
         mu[[i]] <- mu[[i]]*tmpCon
     }
@@ -32,10 +34,10 @@ doit <- function() {
 
     pb <- txtProgressBar(style = 3)
     
-    jCX <- rep(0, 500)
-    jNEW <- rep(0, 500)
-    jSC <- rep(0, 500)
-    for (myIterator in 1:500) {
+    jCX <- rep(0, 1000)
+    jNEW <- rep(0, 1000)
+    jSC <- rep(0, 1000)
+    for (myIterator in 1:1000) {
         X <- lapply(1:K, function(k) {
             dataGen(n[k], p, mu[[k]], Sigma)
         })
@@ -70,10 +72,11 @@ doit <- function() {
         if ((sum(tmpSCstat >= theSCstat) + 1) / (B + 1) <= 0.05)
             jSC[myIterator] <- 1
         
-        setTxtProgressBar(pb, myIterator / 500)
+        setTxtProgressBar(pb, myIterator / 1000)
     }
     close(pb)
     return(data.frame(
+        SNR=SNR,
         CX = mean(jCX),
         SC = mean(jSC),
         NEW = mean(jNEW)
@@ -81,21 +84,31 @@ doit <- function() {
 }
 
 ######################################################
-B = 100
-# sample number
-K = 3
-n <- c(10, 10, 10)
-p = 80
-Sigma <- diag(p)
-Sigma[1,1]<-100
+tmpList<-NULL
+for(SNR in seq(0,10)){
+    B = 100
+    # sample number
+    K = 3
+    n <- c(10, 10, 10)
+    p = 100
+    Sigma <- diag(p)
+    Sigma[1,1]<- p
+    
+    mu <- list(rep(1, p), rep(-1, p), rep(0, p))
+    #SNR <- 0
+    
+    (tmpFram<-doit())
+    tmpList<-c(tmpList,list(tmpFram))
+}
 
-mu <- list(rep(1, p), rep(0, p), rep(0, p))
-SNR <- 0.0
-
-(doit())
-
+theOut<-do.call(rbind,tmpList)
+write.csv(theOut,"1p100.csv",row.names=FALSE)
+ 
 #########################################################################
 
+a<-read.csv("1p50.csv")
+b<-read.csv("1p75.csv")
+c<-read.csv("1p100.csv")
 
 #########################################################################
 ##### plot(ecdf(jjj))
