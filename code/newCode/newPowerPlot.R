6dd7c9caebbf0d904ceb6eb389670cd69edf1235
kfeng123 <446100240@qq.com>
2018-07-16 22:05:41 +0800

    NEAR

--- code/newCode/newPowerPlot.R
+++ code/newCode/newPowerPlot.R
@@ -84,15 +84,26 @@ tLFD <- data.frame(
 )
 df=rbind(tSC,tCX,tHBWW,tZGZ,tLFD)
 
+cols <- c("Sc"="red","LFD"="magenta","CX"="orange1","HBWW"="green3","ZGZ"="lightskyblue2")
+lineT <- c("Sc"=2,"LFD"=1,"CX"=3,"HBWW"=4,"ZGZ"=5)
+Labels <- c("Sc"="Sc","LFD"="LFD","CX"="CX","HBWW"="HBWW","ZGZ"="ZGZ")
+g <- guide_legend("Test methods",keywidth=unit(1,"cm"),override.aes=list(shape=NA))
 library(ggplot2)
 library(latex2exp)
-myPlot <- ggplot(df,aes(rho,power,colour=method))+
+myPlot <- ggplot(df,aes(rho,power,colour=method,linetype=method))+
     geom_point()+
     geom_line()+
+    scale_linetype_manual(name="Methods",values=lineT,labels=Labels)+
+    scale_colour_manual(name="Methods",values=cols,labels=Labels)+
     ylab("Empirical power")+
     xlab(TeX('$\\rho$'))+
     scale_x_continuous(breaks=seq(0,0.9,0.1))+
-    guides(colour=guide_legend(title=NULL))+
-    theme_bw()
+    theme_bw()+
+    guides(colour=g,linetype=g)+
+    theme(
+        legend.position= c(0.9,0.4),
+        legend.margin = margin(3,3,3,3),
+        legend.box.background = element_rect()
+    )
 
 ggsave("figure1.eps",myPlot)
